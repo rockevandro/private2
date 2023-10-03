47b6bdbcc91534d718535b953fb8ad283690f331
Phillip Freitas <phillipfreitas@mb-dj5rw64h20.home>
2023-10-03 23:02:35 +0100

    Make request type json

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import userRoutes from './routes/UserRoutes';
+import { MongoDBConnection } from "./config/MongoDbConnection";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(userRoutes);
 
-app.listen(port, () => {
-    console.log(`Servidor Express está ouvindo na porta ${port}, acesse: http://localhost:${port}`)
-})
+(async function () {
+    const mongooseConnection = new MongoDBConnection();
+
+    await mongooseConnection.connect();
+
+    app.listen(port, () => {
+        console.log(`Servidor Express está ouvindo na porta ${port}, acesse: http://localhost:${port}`);
+    });
+})();
+
